d5036015f8f1fb8e4511e9d8235efadebb4406aa
Prado Nicolas <nicolas88prado@gmail.com>
2024-09-16 16:02:00 -0300

    agregue el maquetado necesario para el componenete y agregue validaciones basicas con hookform

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Login from './components/pages/Login'
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
  
   return (
     <>
-    
+    <Login></Login>
     </>
   )
 }
